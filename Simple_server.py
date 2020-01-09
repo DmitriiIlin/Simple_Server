d9636842d6772c4dcd2d618ddda30ce426270ac6
Dmitrii_56 <ilin.dmitrii1984@gmail.com>
2020-01-09 13:37:17 +0300

    Простой сервер v.1

---
+++ Simple_server.py
@@ -0,0 +1,27 @@
+from socketserver import BaseRequestHandler, TCPServer
+from socket import socket, AF_INET, SOCK_STREAM
+
+class TestTCPHandler(BaseRequestHandler):
+
+    def handle(self):
+        print("handle activated", self.client_address)
+        self.data = self.request.recv(1024).strip()
+        print(self.data)
+        self.request.send(b'privet, hello')
+
+
+
+def server_active():
+    #Активация сервера
+    server = TCPServer(('localhost', 12345), TestTCPHandler)
+    server.serve_forever()
+    
+def client():
+    #Создание клиента сервера
+    s = socket(AF_INET, SOCK_STREAM)
+    s.connect(('localhost', 12344))
+    s.send(b'Hello')
+    print(s.recv(1024).strip())
+
+server_active()
+client()
